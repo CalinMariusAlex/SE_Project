a2159a9a2d35a30d4e2e230b092d91b6e7e38761
CalinMarius <148520965+CalinMariusAlex@users.noreply.github.com>
2025-04-17 23:10:09 +0300

    Setup and test
    Ar trebui sa vada serverul si baza de date

---
+++ SE_Project/Controllers/UsersController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SE_Project.Data;
+using SE_Project.Models;
+
+namespace SE_Project.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly MyAppContext _context;
+
+        public UsersController(MyAppContext context)
+        {  
+            _context = context; 
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = await _context.Users.ToListAsync();
+            return View(user);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Email")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            return View(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Email")] User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.Id == id);
+            return View(user);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                _context.Users.Remove(user);
+                await _context.SaveChangesAsync();
+
+            }
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ SE_Project/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SE_Project.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [MaxLength(255)]
+        public  string Name { get; set; }
+
+        [MaxLength(255)]
+        public  string Email { get; set; }
+    }
+}
