8b96b01b829365c8495a47255ae1801395a746ce
CalinMarius <148520965+CalinMariusAlex@users.noreply.github.com>
2025-05-27 17:23:50 +0300

    xunit init

---
+++ UnitProject.Tests/PlaylistTests.cs
@@ -0,0 +1,32 @@
+﻿using SE_Project.Models;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace UnitProject.Tests
+{
+    public class PlaylistTests
+    {
+        [Fact]
+        public void Playlist_Creation_SetsCorrectDefaults()
+        {
+            // Arrange
+            var playlist = new Playlist
+            {
+                Name = "Test Playlist",
+                Description = "Test Desc",
+                DateCreated = DateTime.Now,
+                LastModified = DateTime.Now,
+                IsPublic = true
+            };
+
+            // Act & Assert
+            Assert.Equal("Test Playlist", playlist.Name);
+            Assert.True(playlist.IsPublic);
+        }
+    }
+}
---
+++ UnitProject.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitProject.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
